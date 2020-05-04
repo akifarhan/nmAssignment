28e5811eb3747e257b099e45aead1c25f89fc483
Muhammad Akif Farhan <akifarhan@gmail.com>
2020-05-04 11:33:26 +0800

    Add features find location and append data points.
    #TODO - Check append data points type. Might have error

--- Newton_Interpolation.py
+++ Newton_Interpolation.py
@@ -1,18 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
-
-
 import numpy as np
 from fractions import Fraction as frac
 import time
 
-
-# In[2]:
-
-
-## =============  Functions  ==================================
 def getConstant(x,y):
 	n = np.shape(y)[0]
 	pyramid = np.zeros([n,n])
@@ -24,8 +16,15 @@ def getConstant(x,y):
 	return pyramid[0]
 
 
-# In[3]:
-
+def addArray(arr,new_value):
+  try:
+			if '/' in new_value:
+			  new_value = frac(new_value)
+			else:
+			  new_value = int(new_value)
+  except:
+    new_value = frac(new_value).limit_denominator()
+  return np.append(arr,  new_value)
 
 def changeArrayType(arr):
 	"""
@@ -42,9 +41,6 @@ def changeArrayType(arr):
 		except:
 			arr[i] = frac(arr[i]).limit_denominator()
 	return arr
-
-
-# In[4]:
 
 
 def getFormula(constant_vector):
@@ -86,9 +82,6 @@ def getFormula(constant_vector):
 	return formula
 
 
-# In[5]:
-
-
 def findnewX(j):
     total = 0
     for index in range (len(x) - 1):
@@ -105,6 +98,56 @@ def findnewX(j):
         print("Value of X at that location:", x[int(j)])
         return x[int(j)]
 
+def displayResult():
+    ## Get Constant Vector
+  start_time = time.time()
+  constant_vector = getConstant(x,y)
+  print('The Constant Vector = ',constant_vector)
+
+  # Get value Y from X based on Polynomial Equation
+  formula = getFormula(constant_vector)
+  print('')
+  print(f"Excecution Time: {time.time()-start_time} seconds ")
+  test1 = ''.join(formula).replace('(','*(').replace('x','a').replace('c*','c')
+  print(' ')
+
+  # expression to be evaluated
+  expr = test1
+  while True:
+      # variable used in expression
+      selection = input("Enter '1' for value of X, '2' for location of x, 'f' for skip: ")
+      print(selection)
+      if selection == '1':
+          a = input("Enter the value of x:")
+          try:
+              if '/' in a:
+                a=frac(a)
+              else:
+                a = int(a)
+          except:
+              a = frac(a).limit_denominator()
+      elif selection == '2':
+          loc = float(input("Enter the location of x:"))
+          a = findnewX(loc)
+          try:
+              if '/' in a:
+                a=frac(a)
+              else:
+                a = int(a)
+          except:
+              a = frac(a).limit_denominator()
+      elif selection == 'f':
+        break;
+      # evaluating expression
+      Y = eval(expr)
+
+      print('Y =', Y)
+
+      ans = input("Do you want to test with other value of x or other location of x? y/n : ")
+      if ans =='y':
+          continue
+      else:
+          break
 
 # In[6]:
 
@@ -125,58 +168,25 @@ while True:
     x = np.array(changeArrayType(x))
     y = np.array(changeArrayType(y))
 
-    ## Get Constant Vector
-    start_time = time.time()
-    constant_vector = getConstant(x,y)
-    print('The Constant Vector = ',constant_vector)
+    displayResult()
 
-    # Get value Y from X based on Polynomial Equation
-    formula = getFormula(constant_vector)
-    print('')
-    print(f"Excecution Time: {time.time()-start_time} seconds ")
-    test1 = ''.join(formula).replace('(','*(').replace('x','a').replace('c*','c')
-    print(' ')
-    # expression to be evaluated
-    expr = test1
     while True:
-        # variable used in expression
-        selection = input("Press '1' for value of X, '2' for location of x: ")
-        print(selection)
-        if selection == '1':
-            a = input("Enter the value of x:")
-            try:
-                if '/' in a:
-                  a=frac(a)
-                else:
-                  a = int(a)
-            except:
-                a = frac(a).limit_denominator()
-        elif selection == '2':
-            loc = float(input("Enter the location of x:"))
-            a = findnewX(loc)
-            try:
-                if '/' in a:
-                  a=frac(a)
-                else:
-                  a = int(a)
-            except:
-                a = frac(a).limit_denominator()
+      ans = input("Do you want to add data points? y/n : ")
+      if ans =='y':
+        b = input("Enter the value of x: ")
+        x = addArray(x, b)
+        c = input("Enter the value of y: ")
+        y = addArray(y, c)
+        displayResult()
+      else:
+          break
 
-        # evaluating expression
-        Y = eval(expr)
-
-        print('Y =', Y)
-
-        ans = input("Do you want to test with other value of x or other location of x? y/n : ")
-        if ans =='y':
-            continue
-        else:
-            break
     ans = input("Do you want to try with other data points? y/n : ")
-    if ans =='y':
-        continue
+    if ans == 'y':
+      continue
     else:
-        break
+      break
+
 
 
 
